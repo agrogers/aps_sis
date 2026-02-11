62ff82c96f3f6497ac9cc4a1645c6edf670db88e
Andrew Rogers <rogers.ag@gmail.com>
2026-02-11 14:46:04 +0630

    Update aps_resource_types.py

--- models/aps_resource_types.py
+++ models/aps_resource_types.py
@@ -8,6 +8,11 @@ class APSResourceType(models.Model):
     sequence = fields.Integer(string='Sequence', default=10)
     name = fields.Char(string='Name', required=True)
     description = fields.Text(string='Description')
-    icon = fields.Binary(string='Icon', attachment=True)
+    icon = fields.Image(
+        string='Icon',
+        max_width=64,
+        max_height=64,
+        help='Icon for the resource type (e.g., for visual identification in lists)'
+    )
     color = fields.Char(string='Color', help='CSS color for the ribbon (e.g., #17a2b8, red, rgb(0,128,0))')
     resource_ids = fields.One2many('aps.resources', 'type_id', string='Resources')
